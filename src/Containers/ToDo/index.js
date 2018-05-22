c1a6ba418b45ffcce4a59302f4e094a7768f9bda
sowatever <ian781203@hotmail.com>
2018-05-22 09:52:14 -0700

    remove console.log

--- src/Containers/ToDo/index.js
+++ src/Containers/ToDo/index.js
@@ -3,11 +3,10 @@ import ToDo from '../../Components/ToDo'
 import {actions} from './action_reducer'
 
 export default connect(
-    state => {console.log(state.toDo)
-        return {
+    state    => ({
         input: state.toDo.input,
         tasks: state.toDo.tasks
-    }},
+    }),
     dispatch => ({
        
         onChange:      e => dispatch(actions.updateInput(e)),
